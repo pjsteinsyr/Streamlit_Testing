a8a7adab851461594ad1400dc5120ae97abbd0fb
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-07 16:55:21 -0500

    Add files via upload

--- Streamlit_Test.py
+++ Streamlit_Test.py
@@ -2,10 +2,12 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-rand=np.random.normal(1, 2, size=20)fig, ax = plt.subplots()ax.hist(rand, bins=15)st.pyplot(fig)
+
 st.title("Peter's Test Site")
 st.header("**This site is a test for IST 300**")
 st.image("SyracuseUniversity.jpg")
 st.radio('Pick your gender',['Male','Female'])
-
+rand=np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()ax.hist(rand, bins=15)
+st.pyplot(fig)
 
