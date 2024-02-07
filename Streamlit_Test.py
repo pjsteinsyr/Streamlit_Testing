074267261a3839e6a7f34888cba948eda9d4d950
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-07 16:51:58 -0500

    Add files via upload

--- Streamlit_Test.py
+++ Streamlit_Test.py
@@ -3,5 +3,5 @@ st.title("Peter's Test Site")
 st.header("**This site is a test for IST 300**")
 st.image("SyracuseUniversity.jpg")
 st.radio('Pick your gender',['Male','Female'])
-st.number_input('Pick a number', 0,10)
-st.balloons()
+with st.number_input('Pick a number', 0,10):
+    st.balloons()
