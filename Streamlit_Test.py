e27329845f07a999455233c986297b4724d3b21e
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-07 16:59:32 -0500

    Add files via upload

--- Streamlit_Test.py
+++ Streamlit_Test.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
 
 st.title("Peter's Test Site")
 st.header("**This site is a test for IST 300**")
 st.image("SyracuseUniversity.jpg")
 st.radio('Pick your gender',['Male','Female'])
-rand=np.random.normal(1, 2, size=20)
-fig, ax = plt.subplots()
-ax.hist(rand, bins=15)
-st.pyplot(fig)
+df= pd.DataFrame(np.random.randn(10, 2),    
+                 columns=['x', 'y'])
+st.line_chart(df)
 
