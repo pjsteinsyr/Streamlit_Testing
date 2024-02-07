a41939a26eaf4571a5ec498c1cbe80532b995d13
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-07 16:47:14 -0500

    Add files via upload

--- Streamlit_Test.py
+++ Streamlit_Test.py
@@ -1,2 +1,5 @@
 import streamlit as st
-st.write('Hello World')
+st.title("Peter's Test Site")
+st.header("##**This site is a test for IST 300**")
+st.image("SyracuseUniversity.jpg")
+st.radio('Pick your gender',['Male','Female'])
