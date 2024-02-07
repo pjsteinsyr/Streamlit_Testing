6fe949cb323dd755546113b7c322f4e8d4d1007a
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-07 16:54:14 -0500

    Add files via upload

--- Streamlit_Test.py
+++ Streamlit_Test.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+rand=np.random.normal(1, 2, size=20)fig, ax = plt.subplots()ax.hist(rand, bins=15)st.pyplot(fig)
 st.title("Peter's Test Site")
 st.header("**This site is a test for IST 300**")
 st.image("SyracuseUniversity.jpg")
 st.radio('Pick your gender',['Male','Female'])
-with st.number_input('Pick a number', 0,10):
-    st.balloons()
+
+
