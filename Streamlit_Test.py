8d20e1ec411dedc3cd7af4be2b1ef903ebdc27bf
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-08 16:44:34 -0500

    Add files via upload

--- Streamlit_Test.py
+++ Streamlit_Test.py
@@ -5,9 +5,12 @@ import pandas as pd
 
 st.title("Peter's Test Site")
 st.header("**This site is a test for IST 300**")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 st.image("SyracuseUniversity.jpg")
 st.radio('Pick your gender',['Male','Female'])
-df= pd.DataFrame(np.random.randn(10, 2),    
+df= pd.DataFrame(np.random.randn(10, 2),
                  columns=['x', 'y'])
 st.line_chart(df)
-
