4bf072d318be63270cf039141b37b47dbde201ce
Peter Stein <96093238+pjsteinsyr@users.noreply.github.com>
2024-02-12 10:32:19 -0500

    Update Streamlit_Test.py

--- Streamlit_Test.py
+++ Streamlit_Test.py
@@ -33,3 +33,6 @@ st.line_chart(df)
 container = st.container()
 container.write("This is my first container!")
 st.graphviz_chart('''digraph {        Big_shark -> Tuna        Tuna -> Mackerel        Mackerel -> Small_fishes        Small_fishes -> Shrimp }''')
+
+df = pd.DataFrame(np.random.randn(500, 2) / [50, 50] + [37.76, -122.4],columns=['lat', 'lon'])
+st.map(df)
